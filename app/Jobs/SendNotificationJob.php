a2b968321140a870905d7de6e9fb853b2fa2b118
João Marcos Neves da Silva <joaomarcosnina@gmail.com>
2024-07-05 10:09:13 -0300

    feat: send notification by dispatch job

--- app/Jobs/SendNotificationJob.php
+++ app/Jobs/SendNotificationJob.php
@@ -2,8 +2,11 @@
 
 namespace App\Jobs;
 
+use App\Enums\NotificationChannelEnum;
 use App\Enums\NotificationStatusEnum;
-use App\Models\Notification;
+use App\Models\Notification as NotificationModel;
+use Illuminate\Support\Facades\Notification;
+use App\Notifications\NotificationPaid;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
@@ -15,12 +18,12 @@ class SendNotificationJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    protected Notification $notification;
+    protected NotificationModel $notification;
 
     /**
      * Create a new job instance.
      */
-    public function __construct(Notification $notification)
+    public function __construct(NotificationModel $notification)
     {
         $this->notification = $notification;
     }
@@ -31,7 +34,26 @@ class SendNotificationJob implements ShouldQueue
     public function handle(): void
     {
         try {
-            // TODO: Implementar a notificação
+            switch ($this->notification->channel) {
+                case NotificationChannelEnum::EMAIL:
+                    Notification::route('mail', $this->notification->destination)
+                        ->notify(new NotificationPaid($this->notification->message));
+                    break;
+                case NotificationChannelEnum::PUSH:
+                    Log::info('no implemented');
+                    break;
+                case NotificationChannelEnum::SMS:
+                    Log::info('no implemented');
+
+                    break;
+                case NotificationChannelEnum::WHATSAPP:
+                    Log::info('no implemented');
+
+                    break;
+                default:
+                    Log::info('not found');
+                    break;
+            }
             $this->notification->status = NotificationStatusEnum::SUCCESS;
             $this->notification->save();
         } catch (\Exception $e) {
